368a3c0b52c291023d24f55cafc5a01af746012a
SyuA0529 <silbi3408@gmail.com>
2025-04-20 16:58:38 +0900

    init: multi module init

---
+++ domain/order/src/test/java/store/nightmarket/domain/order/OrderApplicationTests.java
@@ -0,0 +1,13 @@
+package store.nightmarket.domain.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
