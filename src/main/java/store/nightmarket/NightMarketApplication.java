4cf042313b81b90a40d41d9f0d6b1d5add15cc54
SyuA0529 <silbi3408@gmail.com>
2025-04-20 16:05:54 +0900

    init

---
+++ src/main/java/store/nightmarket/NightMarketApplication.java
@@ -0,0 +1,13 @@
+package store.nightmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NightMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NightMarketApplication.class, args);
+	}
+
+}
