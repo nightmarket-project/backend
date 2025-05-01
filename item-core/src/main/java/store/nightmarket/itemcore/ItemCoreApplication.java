66fdcaa69f6208523479df936162556778709517
hjkl2 <hjkl2426@naver.com>
2025-05-01 14:42:24 +0900

    init: item-core module init

---
+++ item-core/src/main/java/store/nightmarket/itemcore/ItemCoreApplication.java
@@ -0,0 +1,13 @@
+package store.nightmarket.itemcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItemCoreApplication.class, args);
+    }
+
+}
---
+++ item-core/src/test/java/store/nightmarket/itemcore/ItemCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package store.nightmarket.itemcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
