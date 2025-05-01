aced715591abde34ad62ffdef17880ca80c0df36
hjkl2 <hjkl2426@naver.com>
2025-05-01 14:47:19 +0900

    init: item-web module init

---
+++ domain/item-web/src/test/java/store/nightmarket/itemweb/ItemWebApplicationTests.java
@@ -0,0 +1,13 @@
+package store.nightmarket.itemweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
