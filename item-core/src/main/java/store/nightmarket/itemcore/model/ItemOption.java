2f3f4580f3d7a77cb8eb052bfe41d19a332f8008
hjkl2 <hjkl2426@naver.com>
2025-05-02 16:27:51 +0900

    refactor: ItemOption에 BaseModel 상속 및 테스트 수정

--- item-core/src/main/java/store/nightmarket/itemcore/model/ItemOption.java
+++ item-core/src/main/java/store/nightmarket/itemcore/model/ItemOption.java
@@ -1,28 +1,31 @@
 package store.nightmarket.itemcore.model;
 
 import lombok.Getter;
+import store.nightmarket.common.domain.model.BaseModel;
 import store.nightmarket.itemcore.state.OptionState;
+import store.nightmarket.itemcore.valueobject.ItemOptionId;
 import store.nightmarket.itemcore.valueobject.Name;
 import store.nightmarket.itemcore.valueobject.Price;
 import store.nightmarket.itemcore.valueobject.Quantity;
 
 @Getter
-public class ItemOption {
+public class ItemOption extends BaseModel<ItemOptionId> {
 
     private Name name;
     private Price price;
     private Quantity quantity;
     private OptionState state;
 
-    private ItemOption(Name name, Price price, Quantity quantity, OptionState state) {
+    private ItemOption(ItemOptionId id, Name name, Price price, Quantity quantity, OptionState state) {
+        super(id);
         this.name = name;
         this.price = price;
         this.quantity = quantity;
         this.state = state;
     }
 
-    public static ItemOption create(Name name, Price price, Quantity quantity, OptionState state) {
-        return new ItemOption(name, price, quantity, state);
+    public static ItemOption newInstance(ItemOptionId id, Name name, Price price, Quantity quantity, OptionState state) {
+        return new ItemOption(id, name, price, quantity, state);
     }
 
 }
--- item-core/src/test/java/store/nightmarket/itemcore/model/ItemOptionTest.java
+++ item-core/src/test/java/store/nightmarket/itemcore/model/ItemOptionTest.java
@@ -3,11 +3,13 @@ package store.nightmarket.itemcore.model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import store.nightmarket.itemcore.state.OptionState;
+import store.nightmarket.itemcore.valueobject.ItemOptionId;
 import store.nightmarket.itemcore.valueobject.Name;
 import store.nightmarket.itemcore.valueobject.Price;
 import store.nightmarket.itemcore.valueobject.Quantity;
 
 import java.math.BigDecimal;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -20,8 +22,8 @@ class ItemOptionTest {
 
     @Test
     @DisplayName("ItemOption은 이름, 가격, 수량, 상태로 생성된다")
-    void createItemOptionSuccessfully() {
-        ItemOption itemOption = createItemOption();
+    void newInstanceItemOptionSuccessfully() {
+        ItemOption itemOption = newInstanceItemOption();
 
         assertThat(itemOption).isNotNull();
         assertThat(itemOption.getName()).isEqualTo(NAME);
@@ -30,9 +32,9 @@ class ItemOptionTest {
         assertThat(itemOption.getState()).isEqualTo(STATE);
     }
 
-    private ItemOption createItemOption() {
-        return ItemOption.create(
-                NAME, PRICE, QUANTITY, STATE
+    private ItemOption newInstanceItemOption() {
+        return ItemOption.newInstance(
+                new ItemOptionId(UUID.randomUUID()), NAME, PRICE, QUANTITY, STATE
         );
     }
 
