22292caf61d8d8bbcabed0eb8fc69bb42debf819
sunandrabbit <sunandrabbit@naver.com>
2025-05-07 13:43:12 +0900

    refactor: 객체 비교 책임을 본인이 가지도록 로직 수정

--- domain/order/src/main/java/store/nightmarket/domain/order/model/DetailOrderRecord.java
+++ domain/order/src/main/java/store/nightmarket/domain/order/model/DetailOrderRecord.java
@@ -11,57 +11,62 @@ import store.nightmarket.domain.order.valueobject.Quantity;
 @Getter
 public class DetailOrderRecord extends BaseModel<DetailOrderRecordId> {
 
-    private ProductId productId;
-    private Quantity quantity;
-    private DetailOrderState state;
+	private ProductId productId;
+	private Quantity quantity;
+	private DetailOrderState state;
 
-    public DetailOrderRecord(
-        DetailOrderRecordId id,
-        ProductId productId,
-        Quantity quantity,
-        DetailOrderState state) {
+	public DetailOrderRecord(
+		DetailOrderRecordId id,
+		ProductId productId,
+		Quantity quantity,
+		DetailOrderState state
+	) {
 
-        super(id);
-        this.productId = productId;
-        this.quantity = quantity;
-        this.state = state;
-    }
+		super(id);
+		this.productId = productId;
+		this.quantity = quantity;
+		this.state = state;
+	}
 
-    public static DetailOrderRecord newInstance(
-        DetailOrderRecordId id,
-        ProductId productId,
-        Quantity quantity,
-        DetailOrderState state
-    ) {
+	public static DetailOrderRecord newInstance(
+		DetailOrderRecordId id,
+		ProductId productId,
+		Quantity quantity,
+		DetailOrderState state
+	) {
 
-        return new DetailOrderRecord(
-            id,
-            productId,
-            quantity,
-            state
-        );
-    }
+		return new DetailOrderRecord(
+			id,
+			productId,
+			quantity,
+			state
+		);
+	}
 
-    public void submit() {
-        if(!state.isAbleToChangeToSubmitted()) {
-            throw new OrderException("cannot change state to submitted");
-        }
-        this.state = DetailOrderState.SUBMITTED;
-    }
+	public void submit() {
+		if (!state.isAbleToChangeToSubmitted()) {
+			throw new OrderException("cannot change state to submitted");
+		}
+		this.state = DetailOrderState.SUBMITTED;
+	}
 
-    public void cancel() {
-        if (!state.isAbleToChangeToCanceled()) {
-            throw new OrderException("cannot change state to canceled");
-        }
-        this.state = DetailOrderState.CANCELED;
-    }
+	public void cancel() {
+		if (!state.isAbleToChangeToCanceled()) {
+			throw new OrderException("cannot change state to canceled");
+		}
+		this.state = DetailOrderState.CANCELED;
+	}
 
-    public boolean isSubmitted() {
-        return state.equals(DetailOrderState.SUBMITTED);
-    }
+	public boolean isSubmitted() {
+		return state.equals(DetailOrderState.SUBMITTED);
+	}
 
-    public boolean isCanceled() {
-        return state.equals(DetailOrderState.CANCELED);
-    }
+	public boolean isCanceled() {
+		return state.equals(DetailOrderState.CANCELED);
+	}
+
+	public boolean isMatched(DetailOrderRecord detailOrderRecord) {
+		return getId().equals(detailOrderRecord.getId());
+	}
 
 }
