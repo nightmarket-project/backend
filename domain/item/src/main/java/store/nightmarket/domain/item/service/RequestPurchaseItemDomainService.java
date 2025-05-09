98076a97edd77bd02f9d77528c287309fadf266f
hjkl2 <hjkl2426@naver.com>
2025-05-09 23:31:45 +0900

    refactor: 예외 처리 변경

--- domain/item/src/main/java/store/nightmarket/domain/item/service/RequestPurchaseItemDomainService.java
+++ domain/item/src/main/java/store/nightmarket/domain/item/service/RequestPurchaseItemDomainService.java
@@ -2,6 +2,7 @@ package store.nightmarket.domain.item.service;
 
 import org.springframework.stereotype.Component;
 import store.nightmarket.common.domain.service.BaseDomainService;
+import store.nightmarket.domain.item.exception.ProductItemException;
 import store.nightmarket.domain.item.model.ProductItem;
 import store.nightmarket.domain.item.model.UserProductItem;
 
@@ -16,7 +17,7 @@ public class RequestPurchaseItemDomainService
         UserProductItem buyProductITem = input.getBuyProductItem();
 
         UserProductItem userProductItem = productItem.isAvailableToBuy(buyProductITem)
-                .orElseThrow(() -> new RuntimeException("아이템과 구매 아이템이 다릅니다."));
+                .orElseThrow(() -> new ProductItemException("아이템과 구매 아이템이 다릅니다."));
 
         return Event.builder()
                 .buyProductItem(userProductItem)
