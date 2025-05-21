b872ff821a4f4887bcda0df85382ee94382aa608
sunandrabbit <sunandrabbit@naver.com>
2025-05-21 23:40:42 +0900

    rename: package명 수정

---
+++ application/app-item/src/test/java/store/nightmarket/application/appitem/AppItemApplicationTests.java
@@ -0,0 +1,13 @@
+package store.nightmarket.application.appitem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppItemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ application/app-payment/src/main/java/store/nightmarket/application/apppayment/AppPaymentApplication.java
@@ -0,0 +1,13 @@
+package store.nightmarket.application.apppayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppPaymentApplication.class, args);
+	}
+
+}
