0b5a1083dcf3bd84a04b6505761b64275e1ee70b
sunandrabbit <sunandrabbit@naver.com>
2025-05-21 22:36:01 +0900

    init: 애플리케이션 계층 모듈 추가

---
+++ application/app-item/src/test/java/nightmarket/item/AppItemApplicationTests.java
@@ -0,0 +1,13 @@
+package nightmarket.item;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppItemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
