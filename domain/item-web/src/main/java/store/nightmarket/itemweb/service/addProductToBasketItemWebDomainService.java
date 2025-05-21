004384e8fa52dd0d1905b18c8c8c50334cbfb008
Hojun_Choi <hjkl2426@naver.com>
2025-05-21 17:20:43 +0900

    refactor: item-core 기능 변경으로 인한 수정

--- domain/item-web/src/main/java/store/nightmarket/itemweb/service/addProductToBasketItemWebDomainService.java
+++ domain/item-web/src/main/java/store/nightmarket/itemweb/service/addProductToBasketItemWebDomainService.java
@@ -1,10 +1,13 @@
 package store.nightmarket.itemweb.service;
 
 import store.nightmarket.common.domain.service.BaseDomainService;
+import store.nightmarket.common.util.ItemOptionValidationError;
 import store.nightmarket.domain.item.model.ProductItem;
 import store.nightmarket.domain.item.model.UserBuyProductItem;
 import store.nightmarket.itemweb.exception.ItemWebException;
 import store.nightmarket.itemweb.model.ShoppingBasket;
+
+import java.util.List;
 
 import static store.nightmarket.itemweb.service.dto.addProductToBasketItemWebDomainServiceDto.Event;
 import static store.nightmarket.itemweb.service.dto.addProductToBasketItemWebDomainServiceDto.Input;
@@ -21,13 +24,12 @@ public class addProductToBasketItemWebDomainService implements BaseDomainService
             throw new ItemWebException("Product item id does not match buy product item id");
         }
 
-        productItem.findProductItemErrors(userBuyProductItem)
-                .ifPresentOrElse(
-                        errors -> {
-                            throw new ItemWebException(errors.toString());
-                        },
-                        () -> shoppingBasket.addProductToBasket(userBuyProductItem)
-                );
+        List<ItemOptionValidationError> productItemErrors = productItem.findProductItemErrors(userBuyProductItem);
+        if (!productItemErrors.isEmpty()) {
+            throw new ItemWebException("Product item does not exist");
+        }
+        shoppingBasket.addProductToBasket(userBuyProductItem);
+
 
         return Event.builder()
                 .shoppingBasket(shoppingBasket)
