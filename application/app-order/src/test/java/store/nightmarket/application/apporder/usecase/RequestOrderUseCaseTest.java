c1552d194e75271a71e92634b51c5297cf3f191a
sunandrabbit <sunandrabbit@naver.com>
2025-06-03 13:24:22 +0900

    refactor: 테스트 데이터 생성 메서드를 Util로 분리

--- application/app-order/src/test/java/store/nightmarket/application/apporder/usecase/RequestOrderUseCaseTest.java
+++ application/app-order/src/test/java/store/nightmarket/application/apporder/usecase/RequestOrderUseCaseTest.java
@@ -1,10 +1,8 @@
 package store.nightmarket.application.apporder.usecase;
 
 import static org.mockito.Mockito.*;
+import static store.nightmarket.application.apporder.util.OrderTestUtil.*;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -13,17 +11,8 @@ import org.junit.jupiter.api.Test;
 
 import store.nightmarket.application.apporder.out.SaveOrderPort;
 import store.nightmarket.application.apporder.usecase.dto.RequestOrderUseCaseDto;
-import store.nightmarket.domain.order.model.DetailOrderRecord;
-import store.nightmarket.domain.order.model.OrderRecord;
 import store.nightmarket.domain.order.service.RequestOrderDomainService;
 import store.nightmarket.domain.order.service.dto.RequestOrderDomainServiceDto;
-import store.nightmarket.domain.order.status.DetailOrderState;
-import store.nightmarket.domain.order.valueobject.Address;
-import store.nightmarket.domain.order.valueobject.DetailOrderRecordId;
-import store.nightmarket.domain.order.valueobject.OrderRecordId;
-import store.nightmarket.domain.order.valueobject.ProductId;
-import store.nightmarket.domain.order.valueobject.Quantity;
-import store.nightmarket.domain.order.valueobject.UserId;
 
 class RequestOrderUseCaseTest {
 
@@ -65,44 +54,5 @@ class RequestOrderUseCaseTest {
 		verify(mockRequestOrderDomainService, times(1))
 			.execute(any());
 	}
-
-	@Test
-	@DisplayName("")
-	void test() {
-		// given
-		// when
-		// then
-	}
-
-	private RequestOrderUseCaseDto.AddressDto makeAddress() {
-		return RequestOrderUseCaseDto.AddressDto.builder()
-			.zipCode("111-111")
-			.roadAddress("구름로 111-111")
-			.detailAddress("111동 111호")
-			.build();
-	}
-
-	private OrderRecord makeOrderRecord() {
-		return OrderRecord.newInstance(
-			new OrderRecordId(UUID.randomUUID()),
-			new Address(
-				"111-111",
-				"구름로 111-111",
-				"111동 111호"
-			),
-			LocalDate.now(),
-			new UserId(UUID.randomUUID()),
-			new ArrayList<>(List.of(makeDetailOrderRecord()))
-		);
-	}
-
-	private DetailOrderRecord makeDetailOrderRecord() {
-		return DetailOrderRecord.newInstance(
-			new DetailOrderRecordId(UUID.randomUUID()),
-			new ProductId(UUID.randomUUID()),
-			new Quantity(1),
-			DetailOrderState.SUBMITTED
-		);
-	}
-
+	
 }
