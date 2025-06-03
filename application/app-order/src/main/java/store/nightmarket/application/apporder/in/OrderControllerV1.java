79b9dfc48877236cbfeb10a030390591d228aaf4
sunandrabbit <sunandrabbit@naver.com>
2025-06-03 12:16:55 +0900

    feat: Controller 구현

---
+++ application/app-order/src/main/java/store/nightmarket/application/apporder/in/OrderControllerV1.java
@@ -0,0 +1,35 @@
+package store.nightmarket.application.apporder.in;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import store.nightmarket.application.apporder.in.dto.SaveOrderDto;
+import store.nightmarket.application.apporder.usecase.RequestOrderUseCase;
+import store.nightmarket.application.apporder.usecase.dto.RequestOrderUseCaseDto;
+
+@RestController
+@RequestMapping("/api/v1/order")
+@RequiredArgsConstructor
+public class OrderControllerV1 {
+
+	private final RequestOrderUseCase requestOrderUseCase;
+
+	@PostMapping
+	public void saveOrder(SaveOrderDto.Request request) {
+		requestOrderUseCase.execute(
+			RequestOrderUseCaseDto.Input.builder()
+				.addressDto(
+					RequestOrderUseCaseDto.AddressDto.builder()
+						.zipCode(request.addressDto().zipCode())
+						.roadAddress(request.addressDto().roadAddress())
+						.detailAddress(request.addressDto().detailAddress())
+						.build()
+				)
+				.userId(request.userId())
+				.build()
+		);
+	}
+
+}
