9a03f3335bd8693745a43c00b859b0cd129087c4
Hojun_Choi <hjkl2426@naver.com>
2025-06-25 14:45:04 +0900

    feature: persist-item 도메인에 item-core 도메인과 관련된 vo entity 구현

---
+++ persistence/persist-item/src/main/java/store/nightmarket/persistence/persistitem/entity/valueobject/QuantityEntity.java
@@ -0,0 +1,19 @@
+package store.nightmarket.persistence.persistitem.entity.valueobject;
+
+import jakarta.persistence.Embeddable;
+import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class QuantityEntity {
+
+    private BigDecimal value;
+
+}
