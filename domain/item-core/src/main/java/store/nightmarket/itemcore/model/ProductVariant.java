4ec28c4f598b8f469115672ee30cf37cb600d612
Hojun_Choi <hjkl2426@naver.com>
2025-06-26 17:09:03 +0900

    feat: item-core 도메인 ProductVariant 필드에 seller, quantity 추가

--- domain/item-core/src/main/java/store/nightmarket/itemcore/model/ProductVariant.java
+++ domain/item-core/src/main/java/store/nightmarket/itemcore/model/ProductVariant.java
@@ -1,39 +1,50 @@
 package store.nightmarket.itemcore.model;
 
-import java.util.ArrayList;
 import java.util.List;
 import store.nightmarket.common.domain.model.BaseModel;
 import store.nightmarket.itemcore.valueobject.ProductId;
 import store.nightmarket.itemcore.valueobject.ProductVariantId;
+import store.nightmarket.itemcore.valueobject.Quantity;
+import store.nightmarket.itemcore.valueobject.UserId;
 
 public class ProductVariant extends BaseModel<ProductVariantId> {
 
     private ProductId productId;
+    private UserId seller;
     private String SKUCode;
+    private Quantity quantity;
     private List<VariantOptionValue> variantOptionValueList;
 
     private ProductVariant(
         ProductVariantId id,
         ProductId productId,
+        UserId seller,
         String SKUCode,
+        Quantity quantity,
         List<VariantOptionValue> variantOptionValueList
     ) {
         super(id);
         this.productId = productId;
+        this.seller = seller;
         this.SKUCode = SKUCode;
+        this.quantity = quantity;
         this.variantOptionValueList = variantOptionValueList;
     }
 
     public ProductVariant newInstance(
         ProductVariantId id,
         ProductId productId,
+        UserId seller,
         String SKUCode,
+        Quantity quantity,
         List<VariantOptionValue> variantOptionValueList
     ) {
         return new ProductVariant(
             id,
             productId,
+            seller,
             SKUCode,
+            quantity,
             variantOptionValueList
         );
     }
