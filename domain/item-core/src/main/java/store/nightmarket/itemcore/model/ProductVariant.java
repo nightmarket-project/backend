e29dff92eff9706db44231e1e757f47dce67f0c9
Hojun_Choi <hjkl2426@naver.com>
2025-06-26 16:31:30 +0900

    feat: item-core 도메인 ProductVariant 클래스 SKUCode 필드 추가

--- domain/item-core/src/main/java/store/nightmarket/itemcore/model/ProductVariant.java
+++ domain/item-core/src/main/java/store/nightmarket/itemcore/model/ProductVariant.java
@@ -9,26 +9,31 @@ import store.nightmarket.itemcore.valueobject.ProductVariantId;
 public class ProductVariant extends BaseModel<ProductVariantId> {
 
     private ProductId productId;
+    private String SKUCode;
     private List<VariantOptionValue> variantOptionValueList;
 
     private ProductVariant(
         ProductVariantId id,
         ProductId productId,
+        String SKUCode,
         List<VariantOptionValue> variantOptionValueList
     ) {
         super(id);
         this.productId = productId;
+        this.SKUCode = SKUCode;
         this.variantOptionValueList = variantOptionValueList;
     }
 
     public ProductVariant newInstance(
         ProductVariantId id,
         ProductId productId,
+        String SKUCode,
         List<VariantOptionValue> variantOptionValueList
     ) {
         return new ProductVariant(
             id,
             productId,
+            SKUCode,
             variantOptionValueList
         );
     }
