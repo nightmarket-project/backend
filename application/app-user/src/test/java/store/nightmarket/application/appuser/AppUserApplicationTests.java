d8cae67af86d892abb7bb0944393e4b434195171
sunandrabbit <sunandrabbit@naver.com>
2025-07-09 17:14:45 +0900

    feat: 기본 설정 구현

---
+++ application/app-user/src/test/java/store/nightmarket/application/appuser/AppUserApplicationTests.java
@@ -0,0 +1,13 @@
+package store.nightmarket.application.appuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
