645acfa9b824369bd1146784de81a538afd22106
Hojun_Choi <hjkl2426@naver.com>
2025-07-11 00:48:29 +0900

    feat: item-web 도메인 모델, vo 구조 변경에 따른 서비스 구조 변경 구현

--- domain/item-web/src/main/java/store/nightmarket/itemweb/service/EditReplyItemWebDomainService.java
+++ domain/item-web/src/main/java/store/nightmarket/itemweb/service/EditReplyItemWebDomainService.java
@@ -5,7 +5,7 @@ import store.nightmarket.domain.item.valueobject.UserId;
 import store.nightmarket.itemweb.model.Reply;
 import store.nightmarket.itemweb.service.dto.EditReplyItemWebDomainServiceDto.Event;
 import store.nightmarket.itemweb.service.dto.EditReplyItemWebDomainServiceDto.Input;
-import store.nightmarket.itemweb.valueobject.ReplyContent;
+import store.nightmarket.itemweb.valueobject.CommentText;
 
 public class EditReplyItemWebDomainService
     implements BaseDomainService<Input, Event> {
@@ -14,9 +14,9 @@ public class EditReplyItemWebDomainService
     public Event execute(Input input) {
         Reply reply = input.getReply();
         UserId userId = input.getUserId();
-        ReplyContent replyContent = input.getReplyContent();
+        CommentText commentText = input.getCommentText();
 
-        reply.edit(userId, replyContent);
+        reply.edit(userId, commentText);
 
         return Event.builder()
             .reply(reply)
