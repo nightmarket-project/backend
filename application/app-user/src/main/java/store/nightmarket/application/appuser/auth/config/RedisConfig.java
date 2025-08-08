8e3728dee7fcec326fd5c65f586f00b3c3ffb1b4
sunandrabbit <sunandrabbit@naver.com>
2025-08-08 19:08:36 +0900

    style: 주석 제거

--- application/app-user/src/main/java/store/nightmarket/application/appuser/auth/config/RedisConfig.java
+++ application/app-user/src/main/java/store/nightmarket/application/appuser/auth/config/RedisConfig.java
@@ -29,24 +29,13 @@ public class RedisConfig {
 	public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
 		ObjectMapper objectMapper = new ObjectMapper();
 
-		// Spring Security 관련 Mixin 등록
 		objectMapper.addMixIn(UsernamePasswordAuthenticationToken.class,
 			UsernamePasswordAuthenticationTokenMixin.class);
 		objectMapper.addMixIn(SimpleGrantedAuthority.class, SimpleGrantedAuthorityMixin.class);
-		// objectMapper.addMixIn(SecurityContextImpl.class, SecurityContextMixin.class);
-		// objectMapper.addMixIn(User.class, UserMixin.class);
-		// objectMapper.addMixIn(Name.class, NameMixin.class);
-		// objectMapper.addMixIn(Point.class, PointMixin.class);
 
-		// JSON 에 있지만 Java 클래스에는 없는 속성이 있어도 에러를 발생시키지 않음
 		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-		// getter 나 setter 가 없으면 직렬화가 안되는데, private 필드에 직접 접근해서 직렬화/역직렬화 가능
 		objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-		// JSON 에 클래스 타입 정보를 포함시켜서 정확한 타입으로 역직렬화
 		objectMapper.activateDefaultTyping(
-			// LaissezFaireSubTypeValidator → "모든 클래스 허용" (관대한 정책)
-			// BasicPolymorphicTypeValidator → 특정 패키지/클래스만 허용 (엄격한 정책)
-			// Spring Security 의 다양한 내부 클래스들이 사용되기 때문에 해당 정책 사용
 			LaissezFaireSubTypeValidator.instance,
 			ObjectMapper.DefaultTyping.NON_FINAL,
 			JsonTypeInfo.As.PROPERTY
@@ -55,11 +44,9 @@ public class RedisConfig {
 		return new GenericJackson2JsonRedisSerializer(objectMapper);
 	}
 
-	// 클래스 속성 사용
 	@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS)
-	// ANY의 경우 private 까지 접근 가능
 	@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
-	@JsonIgnoreProperties({"authenticated"}) // authenticated 속성 무시
+	@JsonIgnoreProperties({"authenticated"})
 	public static abstract class UsernamePasswordAuthenticationTokenMixin {
 
 		@JsonCreator
@@ -71,7 +58,6 @@ public class RedisConfig {
 
 	}
 
-	// SimpleGrantedAuthority Mixin
 	@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 	public static abstract class SimpleGrantedAuthorityMixin {
 
@@ -80,51 +66,5 @@ public class RedisConfig {
 		}
 
 	}
-
-	// // SecurityContext Mixin
-	// @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS)
-	// @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
-	// public static abstract class SecurityContextMixin {
-	//
-	// 	@JsonCreator
-	// 	public SecurityContextMixin(@JsonProperty("authentication") Authentication authentication) {
-	// 	}
-	//
-	// }
-	//
-	// // User Mixin (Domain User 클래스용)
-	// @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
-	// public static abstract class UserMixin {
-	//
-	// 	@JsonCreator
-	// 	public UserMixin(
-	// 		@JsonProperty("name") Name name,
-	// 		@JsonProperty("email") String email,
-	// 		@JsonProperty("imageUrl") String imageUrl,
-	// 		@JsonProperty("point") Point point,
-	// 		@JsonProperty("role") UserRole role,
-	// 		@JsonProperty("authProvider") AuthProvider authProvider,
-	// 		@JsonProperty("providerId") String providerId
-	// 	) {
-	//
-	// 	}
-	// }
-	//
-	// @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
-	// public static abstract class NameMixin {
-	//
-	// 	@JsonCreator
-	// 	public NameMixin(@JsonProperty("value") String value) {
-	// 	}
-	// }
-	//
-	// @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
-	// public static abstract class PointMixin {
-	//
-	// 	@JsonCreator
-	// 	public PointMixin(@JsonProperty("value") Long value) {
-	// 	}
-	//
-	// }
 
 }
