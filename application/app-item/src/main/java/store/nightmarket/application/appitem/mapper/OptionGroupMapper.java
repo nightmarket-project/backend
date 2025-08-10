e1d9262677a41982ecc1791cbc21aa35ac7d9022
Hojun_Choi <hjkl2426@naver.com>
2025-08-10 22:21:17 +0900

    feat: app-item 모듈 OptionGroupMapper, ProductVariantMapper 클래스 toEntity, toDomain 변경

--- application/app-item/src/main/java/store/nightmarket/application/appitem/mapper/OptionGroupMapper.java
+++ application/app-item/src/main/java/store/nightmarket/application/appitem/mapper/OptionGroupMapper.java
@@ -5,30 +5,26 @@ import store.nightmarket.domain.item.valueobject.Name;
 import store.nightmarket.domain.item.valueobject.OptionGroupId;
 import store.nightmarket.domain.item.valueobject.ProductId;
 import store.nightmarket.persistence.persistitem.entity.model.OptionGroupEntity;
-import store.nightmarket.persistence.persistitem.entity.model.ProductEntity;
 import store.nightmarket.persistence.persistitem.entity.valueobject.NameEntity;
 
 public class OptionGroupMapper {
 
-    public static OptionGroup toDomain(OptionGroupEntity entity) {
-        return OptionGroup.newInstance(
-            new OptionGroupId(entity.getId()),
-            new ProductId(entity.getProductEntity().getId()),
-            new Name(entity.getNameEntity().getValue()),
-            entity.getOrder()
-        );
-    }
+	public static OptionGroup toDomain(OptionGroupEntity entity) {
+		return OptionGroup.newInstance(
+			new OptionGroupId(entity.getId()),
+			new ProductId(entity.getProductId()),
+			new Name(entity.getNameEntity().getValue()),
+			entity.getOrder()
+		);
+	}
 
-    public static OptionGroupEntity toEntity(
-        OptionGroup domain,
-        ProductEntity productEntity
-    ) {
-        return OptionGroupEntity.newInstance(
-            domain.getOptionGroupId().getId(),
-            new NameEntity(domain.getName().getValue()),
-            domain.getOrder(),
-            productEntity
-        );
-    }
+	public static OptionGroupEntity toEntity(OptionGroup domain) {
+		return OptionGroupEntity.newInstance(
+			domain.getOptionGroupId().getId(),
+			domain.getProductId().getId(),
+			new NameEntity(domain.getName().getValue()),
+			domain.getOrder()
+		);
+	}
 
 }
--- application/app-item/src/main/java/store/nightmarket/application/appitem/mapper/ProductVariantMapper.java
+++ application/app-item/src/main/java/store/nightmarket/application/appitem/mapper/ProductVariantMapper.java
@@ -5,33 +5,29 @@ import store.nightmarket.domain.item.valueobject.ProductId;
 import store.nightmarket.domain.item.valueobject.ProductVariantId;
 import store.nightmarket.domain.item.valueobject.Quantity;
 import store.nightmarket.domain.item.valueobject.UserId;
-import store.nightmarket.persistence.persistitem.entity.model.ProductEntity;
 import store.nightmarket.persistence.persistitem.entity.model.ProductVariantEntity;
 import store.nightmarket.persistence.persistitem.entity.valueobject.QuantityEntity;
 
 public class ProductVariantMapper {
 
-    public static ProductVariant toDomain(ProductVariantEntity entity) {
-        return ProductVariant.newInstance(
-            new ProductVariantId(entity.getId()),
-            new ProductId(entity.getProductEntity().getId()),
-            new UserId(entity.getUserId()),
-            entity.getSKUCode(),
-            new Quantity(entity.getQuantityEntity().getValue())
-        );
-    }
+	public static ProductVariant toDomain(ProductVariantEntity entity) {
+		return ProductVariant.newInstance(
+			new ProductVariantId(entity.getId()),
+			new ProductId(entity.getProductId()),
+			new UserId(entity.getUserId()),
+			entity.getSKUCode(),
+			new Quantity(entity.getQuantityEntity().getValue())
+		);
+	}
 
-    public static ProductVariantEntity toEntity(
-        ProductVariant domain,
-        ProductEntity productEntity
-    ) {
-        return ProductVariantEntity.newInstance(
-            domain.getProductVariantId().getId(),
-            domain.getProductId().getId(),
-            domain.getSKUCode(),
-            new QuantityEntity(domain.getQuantity().value()),
-            productEntity
-        );
-    }
+	public static ProductVariantEntity toEntity(ProductVariant domain) {
+		return ProductVariantEntity.newInstance(
+			domain.getProductVariantId().getId(),
+			domain.getProductId().getId(),
+			domain.getSeller().getId(),
+			domain.getSKUCode(),
+			new QuantityEntity(domain.getQuantity().value())
+		);
+	}
 
 }
