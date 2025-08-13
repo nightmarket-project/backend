acdaf5e6b9aa8dab32bc3d30cbfba560b0423eb0
Hojun_Choi <hjkl2426@naver.com>
2025-08-13 22:46:49 +0900

    feat: persist-item 모듈 ProductVariantEntity 클래스 shoppingBasketProductEntityList, ProductEntity 필드 삭제 및 생성자 id, ProductId 추가

--- persistence/persist-item/src/main/java/store/nightmarket/persistence/persistitem/entity/model/ProductVariantEntity.java
+++ persistence/persist-item/src/main/java/store/nightmarket/persistence/persistitem/entity/model/ProductVariantEntity.java
@@ -8,8 +8,6 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
@@ -25,6 +23,9 @@ import store.nightmarket.persistence.persistitem.entity.valueobject.QuantityEnti
 public class ProductVariantEntity extends BaseUuidEntity {
 
 	@Column(name = "user_id", columnDefinition = "BINARY(16)", nullable = false)
+	private UUID productId;
+
+	@Column(name = "user_id", columnDefinition = "BINARY(16)", nullable = false)
 	private UUID userId;
 
 	@Column(name = "SKU_code", nullable = false)
@@ -37,36 +38,33 @@ public class ProductVariantEntity extends BaseUuidEntity {
 	@OneToMany(mappedBy = "productVariantEntity", fetch = FetchType.LAZY)
 	private List<VariantOptionValueEntity> variantOptionValueEntityList = new ArrayList<>();
 
-	@OneToMany(mappedBy = "productVariantEntity", fetch = FetchType.LAZY)
-	private List<ShoppingBasketProductEntity> shoppingBasketProductEntityList = new ArrayList<>();
-
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "product_id")
-	private ProductEntity productEntity;
-
 	private ProductVariantEntity(
+		UUID id,
+		UUID productId,
 		UUID userId,
 		String SKUCode,
-		QuantityEntity quantityEntity,
-		ProductEntity productEntity
+		QuantityEntity quantityEntity
 	) {
+		super(id);
+		this.productId = productId;
 		this.userId = userId;
 		this.SKUCode = SKUCode;
 		this.quantityEntity = quantityEntity;
-		this.productEntity = productEntity;
 	}
 
 	public static ProductVariantEntity newInstance(
+		UUID id,
+		UUID productId,
 		UUID userId,
 		String SKUCode,
-		QuantityEntity quantityEntity,
-		ProductEntity productEntity
+		QuantityEntity quantityEntity
 	) {
 		return new ProductVariantEntity(
+			id,
+			productId,
 			userId,
 			SKUCode,
-			quantityEntity,
-			productEntity
+			quantityEntity
 		);
 	}
 
