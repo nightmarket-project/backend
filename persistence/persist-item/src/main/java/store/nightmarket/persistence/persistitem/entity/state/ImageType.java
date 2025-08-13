ed24cf1a71b3ee220af4950c7edca26b18074532
Hojun_Choi <hjkl2426@naver.com>
2025-08-13 23:14:58 +0900

    feat: persist-item 모듈 ImageType 클래스 state 패키지로 이동

---
+++ persistence/persist-item/src/main/java/store/nightmarket/persistence/persistitem/entity/state/ImageType.java
@@ -0,0 +1,9 @@
+package store.nightmarket.persistence.persistitem.entity.state;
+
+public enum ImageType {
+
+	MAIN,
+	THUMBNAIL,
+	DETAIL
+
+}
--- persistence/persist-item/src/main/java/store/nightmarket/persistence/persistitem/entity/valueobject/ImageType.java
+++
@@ -1,9 +0,0 @@
-package store.nightmarket.persistence.persistitem.entity.valueobject;
-
-public enum ImageType {
-
-	MAIN,
-	THUMBNAIL,
-	DETAIL
-
-}
