710be17c0a8d3706a979b5a4079900997df49827
Hojun_Choi <hjkl2426@naver.com>
2025-08-13 22:45:47 +0900

    feat: persist-item 모듈 OptionGroup 클래스 variantOptionValueEntityList, ProductEntity 필드 삭제 및 생성자 id, ProductId 추가

--- persistence/persist-item/src/main/java/store/nightmarket/persistence/persistitem/entity/model/OptionGroupEntity.java
+++ persistence/persist-item/src/main/java/store/nightmarket/persistence/persistitem/entity/model/OptionGroupEntity.java
@@ -2,13 +2,12 @@ package store.nightmarket.persistence.persistitem.entity.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
@@ -23,6 +22,9 @@ import store.nightmarket.persistence.persistitem.entity.valueobject.NameEntity;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OptionGroupEntity extends BaseUuidEntity {
 
+	@Column(name = "product_id", columnDefinition = "BINARY(16)", nullable = false)
+	private UUID productId;
+
 	@Embedded
 	@Column(name = "name")
 	private NameEntity name;
@@ -33,32 +35,29 @@ public class OptionGroupEntity extends BaseUuidEntity {
 	@OneToMany(mappedBy = "optionGroupEntity", fetch = FetchType.LAZY)
 	private List<OptionValueEntity> optionValueEntityList = new ArrayList<>();
 
-	@OneToMany(mappedBy = "optionGroupEntity", fetch = FetchType.LAZY)
-	private List<VariantOptionValueEntity> variantOptionValueEntityList = new ArrayList<>();
-
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "product_id", nullable = false)
-	private ProductEntity productEntity;
-
 	private OptionGroupEntity(
+		UUID id,
+		UUID productId,
 		NameEntity name,
-		int order,
-		ProductEntity productEntity
+		int order
 	) {
+		super(id);
+		this.productId = productId;
 		this.name = name;
 		this.order = order;
-		this.productEntity = productEntity;
 	}
 
 	public static OptionGroupEntity newInstance(
+		UUID id,
+		UUID productId,
 		NameEntity name,
-		int order,
-		ProductEntity productEntity
+		int order
 	) {
 		return new OptionGroupEntity(
+			id,
+			productId,
 			name,
-			order,
-			productEntity
+			order
 		);
 	}
 
