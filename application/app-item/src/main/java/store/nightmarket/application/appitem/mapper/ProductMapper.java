4d13f6a65139e25d99bd0c5afa6f8a539c7c4942
Hojun_Choi <hjkl2426@naver.com>
2025-08-15 02:12:17 +0900

    feat: app-item 모듈 item 도메인과 persist-item 모듈 mapper 클래스 구현

---
+++ application/app-item/src/main/java/store/nightmarket/application/appitem/mapper/ProductMapper.java
@@ -0,0 +1,31 @@
+package store.nightmarket.application.appitem.mapper;
+
+import store.nightmarket.domain.item.model.Product;
+import store.nightmarket.domain.item.valueobject.Name;
+import store.nightmarket.domain.item.valueobject.Price;
+import store.nightmarket.domain.item.valueobject.ProductId;
+import store.nightmarket.persistence.persistitem.entity.model.ProductEntity;
+import store.nightmarket.persistence.persistitem.entity.valueobject.NameEntity;
+import store.nightmarket.persistence.persistitem.entity.valueobject.PriceEntity;
+
+public class ProductMapper {
+
+	public static Product toDomain(ProductEntity entity) {
+		return Product.newInstance(
+			new ProductId(entity.getId()),
+			new Name(entity.getNameEntity().getName()),
+			entity.getDescription(),
+			new Price(entity.getPriceEntity().getAmount())
+		);
+	}
+
+	public static ProductEntity toEntity(Product domain) {
+		return ProductEntity.newInstance(
+			domain.getProductId().getId(),
+			new NameEntity(domain.getName().getValue()),
+			domain.getDescription(),
+			new PriceEntity(domain.getPrice().amount())
+		);
+	}
+	
+}
