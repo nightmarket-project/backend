4b579f7701d17da003224cd9250931d9d2295144
Hojun_Choi <hjkl2426@naver.com>
2025-08-24 16:46:16 +0900

    feat: app-item 모듈 ReadProductPostDto Response 변경

--- application/app-item/src/main/java/store/nightmarket/application/appitem/in/dto/ReadProductPostDto.java
+++ application/app-item/src/main/java/store/nightmarket/application/appitem/in/dto/ReadProductPostDto.java
@@ -3,17 +3,15 @@ package store.nightmarket.application.appitem.in.dto;
 import java.util.List;
 
 import lombok.Builder;
-import store.nightmarket.application.appitem.out.dto.OptionGroupDto;
-import store.nightmarket.application.appitem.out.dto.ProductPostDto;
-import store.nightmarket.application.appitem.out.dto.ProductVariantDto;
 
 public class ReadProductPostDto {
 
 	@Builder
 	public record Response(
-		ProductPostDto productPostDto,
-		List<OptionGroupDto> optionGroupDtoList,
-		List<ProductVariantDto> productVariantDtoList
+		ProductPostControllerDto productPostControllerDto,
+		List<ReviewReplyControllerDto> reviewReplyControllerDtoList,
+		List<OptionGroupValueControllerDto> optionGroupValueControllerDtoList,
+		List<OptionLayerDto> optionLayerDtoList
 	) {
 
 	}
