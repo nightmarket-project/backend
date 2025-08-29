fbcb873f3a2176ac5bc3aa46568f36de893565af
Hojun_Choi <hjkl2426@naver.com>
2025-08-29 16:46:00 +0900

    feat: app-item 모듈 ReadProductPost dto 변경 및 usecase 변경

--- application/app-item/src/main/java/store/nightmarket/application/appitem/usecase/ReadProductPostUseCase.java
+++ application/app-item/src/main/java/store/nightmarket/application/appitem/usecase/ReadProductPostUseCase.java
@@ -2,18 +2,13 @@ package store.nightmarket.application.appitem.usecase;
 
 import static store.nightmarket.application.appitem.usecase.dto.ReadProductPostUseCaseDto.*;
 
-import java.util.List;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
-import store.nightmarket.application.appitem.out.ReadOptionGroupPort;
 import store.nightmarket.application.appitem.out.ReadProductPostPort;
-import store.nightmarket.application.appitem.out.ReadProductVariantPort;
-import store.nightmarket.application.appitem.out.dto.OptionGroupDto;
 import store.nightmarket.application.appitem.out.dto.ProductPostDto;
-import store.nightmarket.application.appitem.out.dto.ProductVariantDto;
 import store.nightmarket.common.application.usecase.BaseUseCase;
 
 @Service
@@ -21,23 +16,14 @@ import store.nightmarket.common.application.usecase.BaseUseCase;
 public class ReadProductPostUseCase implements BaseUseCase<UUID, Output> {
 
 	private final ReadProductPostPort readProductPostPort;
-	private final ReadOptionGroupPort readOptionGroupPort;
-	private final ReadProductVariantPort readProductVariantPort;
 
 	@Override
 	public Output execute(UUID productPostId) {
 		ProductPostDto productPostDto = readProductPostPort
-			.readOrThrowFetchWithReviewAndReplies(productPostId);
-
-		UUID productId = productPostDto.getProductPost().getProductId().getId();
-
-		List<OptionGroupDto> optionGroupDtoList = readOptionGroupPort.readFetchOptionValue(productId);
-		List<ProductVariantDto> productVariantDtoList = readProductVariantPort.readFetchVariantOptionValue(productId);
-
+			.readOrThrowFetch(productPostId);
+		
 		return Output.builder()
 			.productPostDto(productPostDto)
-			.optionGroupDtoList(optionGroupDtoList)
-			.productVariantDtoList(productVariantDtoList)
 			.build();
 	}
 
--- application/app-item/src/main/java/store/nightmarket/application/appitem/usecase/dto/ReadProductPostUseCaseDto.java
+++ application/app-item/src/main/java/store/nightmarket/application/appitem/usecase/dto/ReadProductPostUseCaseDto.java
@@ -1,19 +1,13 @@
 package store.nightmarket.application.appitem.usecase.dto;
 
-import java.util.List;
-
 import lombok.Builder;
-import store.nightmarket.application.appitem.out.dto.OptionGroupDto;
 import store.nightmarket.application.appitem.out.dto.ProductPostDto;
-import store.nightmarket.application.appitem.out.dto.ProductVariantDto;
 
 public class ReadProductPostUseCaseDto {
 
 	@Builder
 	public record Output(
-		ProductPostDto productPostDto,
-		List<OptionGroupDto> optionGroupDtoList,
-		List<ProductVariantDto> productVariantDtoList
+		ProductPostDto productPostDto
 	) {
 
 	}
