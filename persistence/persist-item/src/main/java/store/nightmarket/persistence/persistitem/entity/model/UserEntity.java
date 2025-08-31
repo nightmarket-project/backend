dd0d10158692aa615da223f4801037b2c157d08a
Hojun_Choi <hjkl2426@naver.com>
2025-08-31 13:18:34 +0900

    feat: item 모듈 User 모델 persist-item 모듈 UserEntity 모델 구현

---
+++ persistence/persist-item/src/main/java/store/nightmarket/persistence/persistitem/entity/model/UserEntity.java
@@ -0,0 +1,55 @@
+package store.nightmarket.persistence.persistitem.entity.model;
+
+import java.util.List;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import store.nightmarket.common.entity.BaseUuidEntity;
+import store.nightmarket.persistence.persistitem.entity.valueobject.NameEntity;
+
+@Getter
+@Entity
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserEntity extends BaseUuidEntity {
+
+	@Embedded
+	@Column(name = "name")
+	private NameEntity name;
+
+	@OneToMany(mappedBy = "userEntity", fetch = FetchType.LAZY)
+	private List<ShoppingBasketProductEntity> shoppingBasketProductEntityList;
+
+	@OneToMany(mappedBy = "userEntity", fetch = FetchType.LAZY)
+	private List<ReviewEntity> reviewEntityList;
+
+	@OneToMany(mappedBy = "userEntity", fetch = FetchType.LAZY)
+	private List<ReplyEntity> replyEntityList;
+
+	public UserEntity(
+		UUID id,
+		NameEntity name
+	) {
+		super(id);
+		this.name = name;
+	}
+
+	public static UserEntity newInstance(
+		UUID id,
+		NameEntity name
+	) {
+		return new UserEntity(
+			id,
+			name
+		);
+	}
+
+}
