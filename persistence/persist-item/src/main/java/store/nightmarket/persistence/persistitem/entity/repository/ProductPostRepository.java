3e969fa7234f3658a969f03c2b665355accabc97
Hojun_Choi <hjkl2426@naver.com>
2025-09-14 06:53:47 +0900

    feat: persist-item 모듈 ProductPostRepository 상품 검색 기능 구현

---
+++ persistence/persist-item/src/main/java/store/nightmarket/persistence/persistitem/entity/repository/ProductPostRepository.java
@@ -0,0 +1,22 @@
+package store.nightmarket.persistence.persistitem.entity.repository;
+
+import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import store.nightmarket.persistence.persistitem.entity.model.ProductPostEntity;
+
+@Repository
+public interface ProductPostRepository extends JpaRepository<ProductPostEntity, UUID> {
+
+	@Query("SELECT productPostEntity FROM ProductPostEntity productPostEntity " +
+		"JOIN FETCH ProductEntity " +
+		"WHERE ProductEntity.nameEntity.name LIKE %:component%")
+	Page<ProductPostEntity> findByComponentContaining(@Param("component") String component, Pageable pageable);
+
+}
