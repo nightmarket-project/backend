18f5d988027ec12cec117b70733ab554e6a90f6a
Hojun_Choi <hjkl2426@naver.com>
2025-09-14 13:50:52 +0900

    feat: app-item 장바구니 상품 수정, 삭제 컨트롤러 구현

--- application/app-item/src/main/java/store/nightmarket/application/appitem/in/dto/SaveShoppingBasketProductDto.java
+++ application/app-item/src/main/java/store/nightmarket/application/appitem/in/dto/SaveShoppingBasketProductDto.java
@@ -1,21 +1,19 @@
 package store.nightmarket.application.appitem.in.dto;
 
+import java.math.BigDecimal;
+import java.util.UUID;
+
 import lombok.Builder;
-import store.nightmarket.domain.item.valueobject.Name;
-import store.nightmarket.domain.item.valueobject.Price;
-import store.nightmarket.domain.item.valueobject.ProductVariantId;
-import store.nightmarket.domain.item.valueobject.Quantity;
-import store.nightmarket.domain.item.valueobject.UserId;
 
 public class SaveShoppingBasketProductDto {
 
 	@Builder
 	public record Request(
-		ProductVariantId productVariantId,
-		UserId userId,
-		Name name,
-		Price price,
-		Quantity quantity
+		UUID productVariantId,
+		UUID userId,
+		String name,
+		BigDecimal price,
+		BigDecimal quantity
 	) {
 
 	}
