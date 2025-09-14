4744c38095416d314a51eba868143bcc79632556
Hojun_Choi <hjkl2426@naver.com>
2025-09-14 13:51:54 +0900

    feat: item-web 도메인 PutProductIntoShoppingBasketItemWebDomainService component 어노테이션 추가

--- domain/item-web/src/main/java/store/nightmarket/itemweb/service/PutProductIntoShoppingBasketItemWebDomainService.java
+++ domain/item-web/src/main/java/store/nightmarket/itemweb/service/PutProductIntoShoppingBasketItemWebDomainService.java
@@ -1,5 +1,7 @@
 package store.nightmarket.itemweb.service;
 
+import org.springframework.stereotype.Component;
+
 import store.nightmarket.common.domain.service.BaseDomainService;
 import store.nightmarket.domain.item.model.ShoppingBasketProduct;
 import store.nightmarket.domain.item.valueobject.Quantity;
@@ -7,20 +9,21 @@ import store.nightmarket.domain.item.valueobject.UserId;
 import store.nightmarket.itemweb.service.dto.PutProductIntoShoppingBasketItemWebDomainServiceDto.Event;
 import store.nightmarket.itemweb.service.dto.PutProductIntoShoppingBasketItemWebDomainServiceDto.Input;
 
+@Component
 public class PutProductIntoShoppingBasketItemWebDomainService
-    implements BaseDomainService<Input, Event> {
+	implements BaseDomainService<Input, Event> {
 
-    @Override
-    public Event execute(Input input) {
-        ShoppingBasketProduct shoppingBasketProduct = input.getShoppingBasketProduct();
-        UserId userId = input.getUserId();
-        Quantity quantity = input.getQuantity();
+	@Override
+	public Event execute(Input input) {
+		ShoppingBasketProduct shoppingBasketProduct = input.getShoppingBasketProduct();
+		UserId userId = input.getUserId();
+		Quantity quantity = input.getQuantity();
 
-        shoppingBasketProduct.changeQuantity(userId, quantity);
+		shoppingBasketProduct.changeQuantity(userId, quantity);
 
-        return Event.builder()
-            .shoppingBasketProduct(shoppingBasketProduct)
-            .build();
-    }
+		return Event.builder()
+			.shoppingBasketProduct(shoppingBasketProduct)
+			.build();
+	}
 
 }
