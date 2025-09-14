e0f1a76abe5d5d2d775bc342aed5eb38ba8c4fdc
Hojun_Choi <hjkl2426@naver.com>
2025-09-14 13:50:29 +0900

    feat: app-item 장바구니 상품 수량 수정 유스케이스 및 port,adaptor 구현

---
+++ application/app-item/src/main/java/store/nightmarket/application/appitem/usecase/dto/ModifyShoppingBasketQuantityUseCaseDto.java
@@ -0,0 +1,19 @@
+package store.nightmarket.application.appitem.usecase.dto;
+
+import lombok.Builder;
+import store.nightmarket.domain.item.valueobject.Quantity;
+import store.nightmarket.domain.item.valueobject.ShoppingBasketProductId;
+import store.nightmarket.domain.item.valueobject.UserId;
+
+public class ModifyShoppingBasketQuantityUseCaseDto {
+
+	@Builder
+	public record Input(
+		ShoppingBasketProductId shoppingBasketProductId,
+		UserId userId,
+		Quantity quantity
+	) {
+
+	}
+
+}
