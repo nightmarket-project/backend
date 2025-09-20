d6aa17c93bc8e2b0b7f3952ad6e0dcbabc942f4c
Hojun_Choi <hjkl2426@naver.com>
2025-09-20 22:56:30 +0900

    refactor: app-item AppItemApplication EnableJpaRepositories,EntityScan 어노테이션 추가

--- application/app-item/src/main/java/store/nightmarket/application/appitem/AppItemApplication.java
+++ application/app-item/src/main/java/store/nightmarket/application/appitem/AppItemApplication.java
@@ -2,9 +2,13 @@ package store.nightmarket.application.appitem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "store.nightmarket.persistence.persistitem")
+@EntityScan(basePackages = "store.nightmarket.persistence.persistitem")
 @ComponentScan(basePackages = {
 	"store.nightmarket.persistence.persistitem",
 	"store.nightmarket.application.appitem",
