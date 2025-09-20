25ee127e31d20846d09474beae9f10228457f4ad
Hojun_Choi <hjkl2426@naver.com>
2025-09-20 20:44:21 +0900

    fix: AppItemApplication ComponentScan 범위 수정

--- application/app-item/src/main/java/store/nightmarket/application/appitem/AppItemApplication.java
+++ application/app-item/src/main/java/store/nightmarket/application/appitem/AppItemApplication.java
@@ -7,10 +7,8 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages = {
 	"store.nightmarket.persistence.persistitem",
-	"store.nightmarket.application.appitem"
 	"store.nightmarket.application.appitem",
 	"store.nightmarket.itemweb"
-	"store.nightmarket.application.appitem"
 })
 public class AppItemApplication {
 
