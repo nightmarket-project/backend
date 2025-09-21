934feb9af731eac283a178dde90eb8bc0c31652b
Hojun_Choi <hjkl2426@naver.com>
2025-09-21 11:46:07 +0900

    refactor: item 도메인 Price 예외 처리 조건 변경

--- domain/item/src/main/java/store/nightmarket/domain/item/valueobject/Price.java
+++ domain/item/src/main/java/store/nightmarket/domain/item/valueobject/Price.java
@@ -2,46 +2,47 @@ package store.nightmarket.domain.item.valueobject;
 
 import java.math.BigDecimal;
 import java.util.Objects;
+
 import store.nightmarket.domain.item.exception.PriceException;
 
 public record Price(BigDecimal amount) {
 
-    public Price {
-        validate(amount);
-    }
+	public Price {
+		validate(amount);
+	}
 
-    private void validate(BigDecimal amount) {
-        if (amount == null) {
-            throw new PriceException("Amount cannot be null");
-        }
+	private void validate(BigDecimal amount) {
+		if (amount == null) {
+			throw new PriceException("Amount cannot be null");
+		}
 
-        if (amount.compareTo(BigDecimal.ZERO) <= 0) {
-            throw new PriceException("The price must be greater than zero");
-        }
-    }
+		if (amount.compareTo(BigDecimal.ZERO) < 0) {
+			throw new PriceException("The price must be greater than zero");
+		}
+	}
 
-    public Price add(Price other) {
-        return new Price(amount.add(other.amount));
-    }
+	public Price add(Price other) {
+		return new Price(amount.add(other.amount));
+	}
 
-    public Price subtract(Price other) {
-        return new Price(amount.subtract(other.amount));
-    }
+	public Price subtract(Price other) {
+		return new Price(amount.subtract(other.amount));
+	}
 
-    public Price multiplyQuantity(Quantity quantity) {
-        return new Price(amount.multiply(quantity.value()));
-    }
+	public Price multiplyQuantity(Quantity quantity) {
+		return new Price(amount.multiply(quantity.value()));
+	}
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null || getClass() != obj.getClass()) {
-            return false;
-        }
-        Price other = (Price) obj;
-        return Objects.equals(amount, other.amount);
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null || getClass() != obj.getClass()) {
+			return false;
+		}
+		Price other = (Price)obj;
+		return Objects.equals(amount, other.amount);
+	}
 
 }
