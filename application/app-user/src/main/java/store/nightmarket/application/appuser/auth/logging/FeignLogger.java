92b48991516533a2d3aa7d133eb4fbb4de90eb57
sunandrabbit <sunandrabbit@naver.com>
2025-10-08 04:50:36 +0900

    feat: feign 로깅 구현

---
+++ application/app-user/src/main/java/store/nightmarket/application/appuser/auth/logging/FeignLogger.java
@@ -0,0 +1,99 @@
+package store.nightmarket.application.appuser.auth.logging;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import net.logstash.logback.argument.StructuredArguments;
+
+import feign.Logger;
+import feign.Request;
+import feign.Response;
+import feign.Util;
+import lombok.extern.slf4j.Slf4j;
+import store.nightmarket.logging.model.detail.FeignEvent;
+
+@Slf4j
+public class FeignLogger extends Logger {
+
+	@Override
+	protected void log(String configKey, String format, Object... args) {
+
+	}
+
+	@Override
+	protected void logRequest(String configKey, Level logLevel, Request request) {
+
+		FeignEvent event = FeignEvent.builder()
+			.eventType("FEIGN")
+			.direction("OUT")
+			.feignClient(configKey)
+			.targetService(extractServiceName(request))
+			.uri(request.url())
+			.method(request.httpMethod().name())
+			.headers(flattenHeaders(request.headers()))
+			.body(parseRequestBody(request))
+			.build();
+
+		log.info(">>> FEIGN REQUEST", StructuredArguments.value("event", event));
+	}
+
+	@Override
+	protected Response logAndRebufferResponse(String configKey, Level logLevel, Response response,
+		long elapsedTime) throws IOException {
+		byte[] bodyData = Util.toByteArray(response.body().asInputStream());
+
+		FeignEvent event = FeignEvent.builder()
+			.eventType("FEIGN")
+			.direction("IN")
+			.feignClient(configKey)
+			.targetService(extractServiceName(response.request()))
+			.uri(response.request().url())
+			.method(response.request().httpMethod().name())
+			.headers(flattenHeaders(response.request().headers()))
+			.body(parseResponseBody(response))
+			.status(response.status())
+			.responseTimeMs(elapsedTime)
+			.build();
+
+		log.info(">>> FEIGN RESPONSE", StructuredArguments.value("event", event));
+
+		return response.toBuilder().body(bodyData).build();
+	}
+
+	private String extractServiceName(Request request) {
+		return request.url().split("/")[2]; //ex http://order-service/api/... → order-service
+	}
+
+	private Map<String, Object> flattenHeaders(Map<String, Collection<String>> headers) {
+		return headers.entrySet().stream()
+			.collect(Collectors.toMap(
+				Map.Entry::getKey,
+				Map.Entry::getValue
+			));
+	}
+
+	private Object parseRequestBody(Request request) {
+		if (request.body() == null)
+			return null;
+		try {
+			return new String(request.body(), request.charset());
+		} catch (Exception e) {
+			return "cannot read body";
+		}
+	}
+
+	private Object parseResponseBody(Response response) {
+		if (response == null || response.body() == null)
+			return null;
+
+		try (InputStream inputStream = response.body().asInputStream()) {
+			return new String(inputStream.readAllBytes(), response.charset());
+		} catch (Exception e) {
+			return "cannot read body";
+		}
+	}
+
+}
