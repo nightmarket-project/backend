48b7973596ffe77fcb9614962b14701c809cd33b
Hojun_Choi <hjkl2426@naver.com>
2025-10-09 14:59:19 +0900

    feat: app-item 모듈 GlobalExceptionHandler 구현

---
+++ application/app-item/src/main/java/store/nightmarket/application/appitem/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package store.nightmarket.application.appitem.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import store.nightmarket.itemweb.exception.ItemWebException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(ItemWebException.class)
+	protected ResponseEntity<String> handleItemWebException(ItemWebException e) {
+		return ResponseEntity
+			.status(HttpStatus.BAD_REQUEST)
+			.body(e.getMessage());
+	}
+	
+}
