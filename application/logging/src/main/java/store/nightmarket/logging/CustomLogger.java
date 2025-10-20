f08e00ad08db8bd1c950dc2f7c7723ecd7d9d302
sunandrabbit <sunandrabbit@naver.com>
2025-10-21 00:56:12 +0900

    feat: objectMapper를 JacksonUtils를 사용하도록 수정

--- application/logging/src/main/java/store/nightmarket/logging/CustomLogger.java
+++ application/logging/src/main/java/store/nightmarket/logging/CustomLogger.java
@@ -4,25 +4,24 @@ import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.kafka.support.JacksonUtils;
 
 import net.logstash.logback.argument.StructuredArguments;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import store.nightmarket.logging.model.CommunicationLog;
 
 public class CustomLogger {
 
 	private static final Logger log = LoggerFactory.getLogger(CustomLogger.class);
-	private static final ObjectMapper objectMapper = new ObjectMapper()
-		.registerModule(new JavaTimeModule());
+	private static final ObjectMapper objectMapper = JacksonUtils.enhancedObjectMapper();
 
 	public static void log(CommunicationLog communicationLog) {
 		Map<String, Object> map = objectMapper.convertValue(communicationLog, new TypeReference<>() {
 		});
-		log.info(communicationLog.getEventType().toString(), StructuredArguments.value("event", map));
+		log.info("{} {}", communicationLog.getEventType().toString(), StructuredArguments.value("event", map));
 	}
 
 }
