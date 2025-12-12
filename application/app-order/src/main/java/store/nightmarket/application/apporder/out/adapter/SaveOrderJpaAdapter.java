f00f548b984761e07425441f553d5658a9f0b2fa
sunandrabbit <sunandrabbit@naver.com>
2025-12-12 15:18:45 +0900

    feat: Adapter가 각각의 repo에 저장하도록 수정

--- application/app-order/src/main/java/store/nightmarket/application/apporder/out/adapter/SaveOrderJpaAdapter.java
+++ application/app-order/src/main/java/store/nightmarket/application/apporder/out/adapter/SaveOrderJpaAdapter.java
@@ -4,8 +4,11 @@ import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
 import store.nightmarket.application.apporder.out.SaveOrderPort;
+import store.nightmarket.application.apporder.out.mapper.DetailOrderRecordMapper;
 import store.nightmarket.application.apporder.out.mapper.OrderRecordMapper;
 import store.nightmarket.domain.order.model.OrderRecord;
+import store.nightmarket.persistence.persistorder.entity.model.OrderRecordEntity;
+import store.nightmarket.persistence.persistorder.repository.DetailOrderRecordRepository;
 import store.nightmarket.persistence.persistorder.repository.OrderRecordRepository;
 
 @Component
@@ -13,10 +16,18 @@ import store.nightmarket.persistence.persistorder.repository.OrderRecordReposito
 public class SaveOrderJpaAdapter implements SaveOrderPort {
 
 	private final OrderRecordRepository orderRecordRepository;
+	private final DetailOrderRecordRepository detailOrderRecordRepository;
 
 	@Override
 	public void save(OrderRecord orderRecord) {
-		orderRecordRepository.save(OrderRecordMapper.toEntity(orderRecord));
+		OrderRecordEntity entity = OrderRecordMapper.toEntity(orderRecord);
+		orderRecordRepository.save(entity);
+		detailOrderRecordRepository.saveAll(
+			DetailOrderRecordMapper.toEntityList(
+				orderRecord.getDetailOrderRecordList(),
+				entity
+			)
+		);
 	}
 
 }
