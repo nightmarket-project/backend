f627cb5fc1bd2c1f8bb344f5c5e4b7d0557432ca
sunandrabbit <sunandrabbit@naver.com>
2026-01-15 01:20:44 +0900

    feat: H2-DB를 In-Memory 방식에서 Embedded 방식으로 변경

---
+++ application/app-user/src/main/resources/schema.sql
@@ -0,0 +1,14 @@
+CREATE TABLE users
+(
+    id                VARCHAR(36)   NOT NULL,
+    created_at        datetime     NOT NULL,
+    updated_at        datetime     NOT NULL,
+    email             VARCHAR(255) NOT NULL,
+    profile_image_url VARCHAR(255) NULL,
+    user_role            VARCHAR(255) NOT NULL,
+    auth_provider     VARCHAR(255) NOT NULL,
+    provider_id       VARCHAR(255) NULL,
+    name              VARCHAR(255) NULL,
+    point             BIGINT       NULL,
+    CONSTRAINT pk_users PRIMARY KEY (id)
+);
