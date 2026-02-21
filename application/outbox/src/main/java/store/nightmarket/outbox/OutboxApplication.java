c72e6bbafda476cc8b7ba25d016f8b18332c0c87
sunandrabbit <sunandrabbit@naver.com>
2026-02-21 21:42:19 +0900

    feat: outbox 모듈 init

---
+++ application/outbox/src/main/java/store/nightmarket/outbox/OutboxApplication.java
@@ -0,0 +1,13 @@
+package store.nightmarket.outbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutboxApplication.class, args);
+	}
+
+}
